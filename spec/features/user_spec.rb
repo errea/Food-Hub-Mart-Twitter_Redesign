4ce875ff9e97a5928729438a8ec89961881af198
Eri-Ngozi Okereafor <eri@Eri-Ngozis-MacBook-Pro.local>
2021-06-07 13:01:36 +0100

    Fix issues with feature test

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -2,16 +2,16 @@ require 'rails_helper'
 
 RSpec.feature 'Users', type: :feature do
   before :each do
-    @usr1 = User.create(username: 'deep', fullname: 'Deepesh Kumar')
+    @usr1 = User.create(username: 'errea', fullname: 'Eringozi Okereafor')
     @usr2 = User.create(username: 'twister', fullname: 'Wingsform')
     visit '/sign_in'
-    fill_in 'session_username', with: 'deep'
+    fill_in 'session_username', with: 'errea'
     click_button 'Login'
   end
 
   it 'visit own profile page' do
     visit profile_path(@usr1.username)
-    expect(page).to have_text('ALL BOOKS SUGGESTED BY DEEPESH KUMAR')
+    expect(page).to have_text('ALL FOODS SUGGESTED BY ERINGOZI OKEREAFOR')
   end
 
   it 'visit other profile page' do
